b28d1a558b118b820e886e4c2fe78d0001155707
NicoConstantin <nicoconstantin11@gmail.com>
2021-06-07 14:00:27 -0300

    refactor back structure

--- api/index.js
+++ api/index.js
@@ -1,28 +1,31 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const routes = require('./src/routes/index.js');
+const errorHandler = require ('./src/utils/middlewares/errorHandler.js');
+const setHeaders = require ('./src/utils/middlewares/setHeaders.js');
+const {PORT} =  require('./src/utils/config/index.js');
+const server = express();
 
+server.name = 'API';
+
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev')); //responde en consola cuando hay una request
+server.use(setHeaders);
+
+server.use('/', routes);
+
+// Error catching endware.
+server.use(errorHandler);
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: true })
+.then(() => {
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
+
+module.exports = server
--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const {dbUser,dbPass,dbHost,dbName} = require ('./utils/config/index.js')
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+const sequelize = new Sequelize(`postgres://${dbUser}:${dbPass}@${dbHost}/${dbName}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -30,7 +28,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Modelo } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
