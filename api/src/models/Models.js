36501a0f24a8ee877755699a585776679fa69e91
MaxiSanchez600 <maxsanchezg@gmail.com>
2021-06-09 19:12:03 -0300

    AnadirTagsAl.JsonDePrueba

--- api/src/models/Models.js
+++ api/src/models/Models.js
@@ -1,6 +1,8 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
+  //PRODUCTS Y CATEGORIES
+
 };
+
+
+
