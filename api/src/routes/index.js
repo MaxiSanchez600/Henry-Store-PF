37e5b84d2721d59352ad17a3b4e00d9a1a850096
josuegoyret <fjgoyretsola@gmail.com>
2021-06-10 11:19:13 -0300

    feat/ api src routes/ imports productRouter and sets up its middleware

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const productsRouter = require('./products');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/products', productsRouter);
 
 
 module.exports = router;
