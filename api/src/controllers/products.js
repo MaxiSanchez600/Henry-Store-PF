eef50e7ff7f6a243bad432f509329464bafb7d2a
Josué Goyret <fjgoyretsola@gmail.com>
2021-06-11 11:17:05 -0300

    unchecked filterCreator
    Se creó la función filterCreator para filtrar en la base de datos según diversos requerimientos: no filter, tag, category, sub_category, caracteristic. Función no fue probada.

--- api/src/controllers/products.js
+++ api/src/controllers/products.js
@@ -2,43 +2,119 @@ const { Product } = require('../db');
 const { Sequelize } = require('sequelize');
 const Op = Sequelize.Op;
 
-const filtersCreator = (name, category, size, priceRange, color, sex, type) => {
+const filtersCreator = (tag, category, rangePriceMin, rangePriceMax, type, caracteristics) => {
     var filters = {};
-    var includes = [];
-    if(name !== 'not passed' && category === 'not passed') {
-        filters.include = [
-            ...includes,    //podría quitarse ya que son los primeros e irian vacios
-            {
-                model: Tags,
-                where: {
-                    name_tag: {
-                        [Op.like]: `%${name.toLowerCase()}%`
-                    }
-                },
-                required: true
+    // var includes = [];
+
+    filters.include = [
+        {
+            model: Tags
+        },
+        {
+            model: Categories,
+            include: [
+                {
+                    model: SubCategories
+                }
+            ]
+        },
+        {
+            model: Caracteristics
+        }
+    ];
+    if(tag !== 'not passed') {
+        filters.include[0].where = {
+            name_tag: {
+                [Op.like]: `%${tag.toLowerCase()}%`
             }
-        ];
+        };
+        filters.include[0].required = true;
     }
-    if(name === 'not passed' && category !== 'not passed') {
-        filters.include = [
-            ...includes,    //podría quitarse ya que son los primeros e irian vacios
-            {
-                model: Categories,
-                where: {
-                    name_category: name
-                },
-                required: true
+    if(category !== 'not passed') {
+        filters.include[1].where = {
+            name_category: category
+        };
+        filters.include[1].required = true;
+    }
+    if(rangePriceMin !== 'not passed' && rangePriceMax !== 'not passed') { // checkear si se necesita required: true acá
+        filters.where = {
+            price: {
+                [Op.and]: {
+                    [Op.gte]: parseInt(rangePriceMin),    //checkear si las props de Op estan bien, si no probar con Op.gt y Op.lt
+                    [Op.lte]: parseInt(rangePriceMax)
+                }
             }
-        ];
+        }
     }
-    if(size !== 'not passed') {
-        filters.include = [
-            ...includes,
-            {
-                model: Caracteristics
+    if(rangePriceMin !== 'not passed' && rangePriceMax === 'not passed') { // checkear si se necesita required: true acá
+        filters.where = {
+            price: {
+                [Op.gte]: parseInt(rangePriceMin)   
             }
-        ]
+        }
     }
+    if(rangePriceMin === 'not passed' && rangePriceMax !== 'not passed') { // checkear si se necesita required: true acá
+        filters.where = {
+            price: {
+                [Op.lte]: parseInt(rangePriceMax)
+            }
+        }
+    }
+    if(type !== 'not passed') { // comprobar si se necesita required: true acá
+        filters.include[1].include[0].where = {
+            name_sub_category: type
+        };
+        filters.include[1].include[0].required = true;
+    }
+    if(Object.keys(caracteristics).length > 0) {    //algo asi seria esto, necesita mil chequeos, y si no traer todo y filtrar fuera de la db
+        filters.include[2].where = {
+            name_caracteristic: {
+                [Op.or]: Object.keys(caracteristics)
+            }
+        };
+        filters.include[2].through = {
+            where: {
+                value_caracteristic: Object.values(caracteristics)
+            }
+        };
+        // filters.include[2].required = true; --> ver si esto es necesario
+    }
+
+
+    // if(name !== 'not passed' && category === 'not passed') {
+    //     filters.include = [
+    //         ...includes,    //podría quitarse ya que son los primeros e irian vacios
+    //         {
+    //             model: Tags,
+    //             where: {
+    //                 name_tag: {
+    //                     [Op.like]: `%${name.toLowerCase()}%`
+    //                 }
+    //             },
+    //             required: true
+    //         }
+    //     ];
+    // }
+    // if(name === 'not passed' && category !== 'not passed') {
+    //     filters.include = [
+    //         ...includes,    //podría quitarse ya que son los primeros e irian vacios
+    //         {
+    //             model: Categories,
+    //             where: {
+    //                 name_category: name
+    //             },
+    //             required: true
+    //         }
+    //     ];
+    // }
+    // if(size !== 'not passed') {
+    //     filters.include = [
+    //         ...includes,
+    //         {
+    //             model: Caracteristics
+    //         }
+    //     ]
+    // }
 }
 
 const productController = {
@@ -47,57 +123,56 @@ const productController = {
         lo que me llega desde el front:
         CASO 1:
         req.query = {
-            name: 'henry' ---> busqueda por tags (el tag se compone de la categoria, colores, (sexo) y la desestructuracion del name)
+            tag: 'henry' ---> busqueda por tags (el tag se compone de la categoria, colores, (sexo) y la desestructuracion del name)
             category: undefined --> budqueda por categoría
-            size: 's', --> filtrado en caracteristica
-            priceRange: '10-50', --> filtrado en producto
-            color: 'negro', --> filtrado en caracteristica
-            sex: undefined, --> filtrado en caracteristica
+            rangePriceMin: '10', --> filtrado en producto
+            rangePriceMax: '50', --> filtrado en producto
             type: 'buso' --> filtrado por subcategoria
-            backPage: 1 --> paginacion del back
+            page: 1 --> paginacion del back
             orderType: undefined,
             orderDirection: undefined
+            color: 'negro', --> filtrado en caracteristica ||caracteristicName1 = caracteristicValue1
+            size: 's', --> filtrado en caracteristica ||caracteristicName2 = caracteristicValue2
+            sex: undefined, --> filtrado en caracteristica ||caracteristicName3 = caracteristicValue3
 
         }
 
         CASO 2:
         req.query = {
-            name: undefined ---> busqueda por tags
+            tag: undefined ---> busqueda por tags
             category: ropa --> budqueda por categoría
-            size: undefined, --> filtrado en caracteristica
-            priceRange: undefined, --> filtrado en producto
-            color: undefined, --> filtrado en caracteristica
-            sex: male, --> filtrado en caracteristica
+            rangePriceMin: undefined, --> filtrado en producto
+            rangePriceMax: 50, --> filtrado en producto
             type: undefined --> filtrado por subcategoria
-            backPage: 1 --> paginacion del back
+            page: 1 --> paginacion del back
             orderType: 'price',
             orderDirection: undefined
+            sex: unisex, --> filtrado en caracteristica || caracteristicName1 = caracteristicValue1
         }
         */
         try {
             const {
-                name = 'not passed',
+                tag = 'not passed',
                 category = ' not passed',
-                size = 'not passed',
-                priceRange = 'not passed',
-                color = 'not passed', 
-                sex = 'not passed', 
+                rangePriceMin = 'not passed',
+                rangePriceMax = 'not passed',
                 type = 'not passed',
-                backPage = 1,
-                orderType = 'name',
-                orderDirection = 'ASC'
+                page = 1,
+                orderType = 'not passed',
+                orderDirection = 'not passed',
+                ...caracteristics
             } = req.query;
 
             //seteo el limit y el offset, es decir, la pagina
             const perPage = 10;
-            const current = (backPage * perPage) - perPage;
+            const current = (page * perPage) - perPage;
 
             //si por query no me llega nada devuelvo la pagina 1 del catalogo completo ordenado alfabitamente por nombre
             const products = await Product.findAll({
-                ...filtersCreator(name, category, size, priceRange, color, sex, type),
-                order: [[orderType, orderDirection]],
-                offset: current,
-                limit: perPage
+                ...filtersCreator(tag, category, rangePriceMin, rangePriceMax, type, caracteristics),
+                // order: [[orderType, orderDirection]],
+                // offset: current,
+                // limit: perPage
             });
         } catch (error) {
             return next(error);
