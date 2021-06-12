5c99bf96d62f8e6b5a2746f0bb31d4e5ab9462ee
DANIEL GIL <78768844+dannieowk@users.noreply.github.com>
2021-06-11 19:00:28 -0500

    Redux OK

--- client/src/Redux/actions/actions.js
+++ client/src/Redux/actions/actions.js
@@ -1,1 +1,21 @@
-//ACA VAN LAS ACTION CREATORS
+// ! MODULES
+import axios from "axios";
+
+// ! URLS 
+import {PRODUCTS_URL} from '../../Config/index';
+
+// ! names_Actions
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+
+/* =============== ACTIONS =============== */ 
+// ! GET LIST PRODUCTS
+export function getProducts() {
+  return function (dispatch) {
+    return axios.get(PRODUCTS_URL).then((response) => {
+      dispatch({
+        type: GET_PRODUCTS,
+        payload: response.data,
+      });
+    });
+  };
+}
--- client/src/Redux/reducers/index.js
+++ client/src/Redux/reducers/index.js
@@ -1,12 +1,25 @@
+// ! ACTIONS
+import {GET_PRODUCTS} from '../actions/actions';
+
 
 const initialState = {
-    Inicial: [],
+    recipes: [],
 };
 
-function rootReducer (state = initialState , action) {
-    switch(action.type){
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+       
+        case GET_PRODUCTS:                  // * Receibe all list of products from backend
+            return {
+                ...state,
+                recipes: action.payload,
+            };
 
+        default:                            // * default
+            return {
+                ...state,
+            };
     }
-}
+};
 
 export default rootReducer;
