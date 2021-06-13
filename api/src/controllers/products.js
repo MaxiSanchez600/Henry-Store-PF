827f177072277cd55ed22da4ed5562928dada3f5
Josué Goyret <fjgoyretsola@gmail.com>
2021-06-13 01:55:09 -0300

    adds values_caracteristic (?)

--- api/src/controllers/products.js
+++ api/src/controllers/products.js
@@ -1,4 +1,4 @@
-const { Product, Caracteristic, ProductCaracteristic } = require('../db');
+const { Product, Category, Tag, Caracteristic, Image, ProductCaracteristic } = require('../db');
 const { Sequelize } = require('sequelize');
 const Op = Sequelize.Op;
 const filtersCreator = require('./controllersUtils/products');
@@ -84,16 +84,45 @@ const productController = {
 
             //  luego de haber pasado por todos los filtrados, busco los id de productsId, con un ordenamiento y paginado, o si no devuelvo un array vacío
             if(productsId.length > 0) {
-                const result = await Product.findAll({
+                var result = await Product.findAll({
                     where: {
                         id_product: {
                             [Op.or]: productsId
                         }
                     },
+                    include: [
+                        { model: Tag, attributes: ['id_tag', 'name_tag'], through: { attributes: [] } },
+                        { model: Category, attributes: ['id_category', 'name_category'], through: { attributes: [] } },
+                        { model: Caracteristic, attributes: ['id_caracteristic', 'name_caracteristic'], through: { attributes: ['value_caracteristic'] } },   
+                        { model: Image } // attributes: ['id_image', 'url_image'], through: { attributes: [] }
+                    ],
                     order: [[orderType, orderDirection]],
                     offset: current,
                     limit: perPage
                 });
+                
+                for(let n = 0; n <= result.length - 1; n++) {
+                    for(let o = 0; o <= result[n].Caracteristics.length - 1; o++) {
+                        var allCaracteristicsProduct = await ProductCaracteristic.findAll({
+                            where: {
+                                ProductIdProduct: result[n].id_product,
+                                CaracteristicIdCaracteristic: result[n].Caracteristics[o].id_caracteristic
+                            }
+                        });
+                        for(let p = 0; p <= allCaracteristicsProduct.length - 1; p++) {
+                            if(p === 0) {
+                                result[n].Caracteristics[o].values_caracteristic = [allCaracteristicsProduct[p].value_caracteristic];
+                            } else {
+                                result[n].Caracteristics[o].values_caracteristic.push(allCaracteristicsProduct[p].value_caracteristic);
+                            }
+                        }
+                    }
+                }
+                // res.send(result[0].Caracteristics[0].values_caracteristic);
+
+                // const palantir = await ProductCaracteristic.findAll();
+                // // console.log(palantir);
+
                 return res.send(result);
             }
             return res.send([]);
@@ -105,3 +134,93 @@ const productController = {
 };
 
 module.exports = productController;
+
+/**
+[
+  {
+    "idProductCaracteristic": 1,
+    "value_caracteristic": "L",
+    "createdAt": "2021-06-11T15:08:14.000Z",
+    "updatedAt": "2021-06-11T15:08:15.000Z",
+    "ProductIdProduct": 1,
+    "CaracteristicIdCaracteristic": 2
+  },
+  {
+    "idProductCaracteristic": 4,
+    "value_caracteristic": "M",
+    "createdAt": "2021-06-11T15:08:32.000Z",
+    "updatedAt": "2021-06-11T15:08:32.000Z",
+    "ProductIdProduct": 1,
+    "CaracteristicIdCaracteristic": 2
+  },
+  {
+    "idProductCaracteristic": 7,
+    "value_caracteristic": "Rojo",
+    "createdAt": "2021-06-11T15:08:54.000Z",
+    "updatedAt": "2021-06-11T15:08:55.000Z",
+    "ProductIdProduct": 1,
+    "CaracteristicIdCaracteristic": 1
+  },
+  {
+    "idProductCaracteristic": 11,
+    "value_caracteristic": "Unisex",
+    "createdAt": "2021-06-11T15:09:24.000Z",
+    "updatedAt": "2021-06-11T15:09:25.000Z",
+    "ProductIdProduct": 1,
+    "CaracteristicIdCaracteristic": 3
+  },
+  {
+    "idProductCaracteristic": 17,
+    "value_caracteristic": "3",
+    "createdAt": "2021-06-11T15:17:25.000Z",
+    "updatedAt": "2021-06-11T15:17:26.000Z",
+    "ProductIdProduct": 1,
+    "CaracteristicIdCaracteristic": 4
+  },
+  {
+    "idProductCaracteristic": 23,
+    "value_caracteristic": "Blanco",
+    "createdAt": "2021-06-11T15:25:13.000Z",
+    "updatedAt": "2021-06-11T15:25:14.000Z",
+    "ProductIdProduct": 3,
+    "CaracteristicIdCaracteristic": 1
+  },
+  {
+    "idProductCaracteristic": 30,
+    "value_caracteristic": "Negro",
+    "createdAt": "2021-06-11T15:25:26.000Z",
+    "updatedAt": "2021-06-11T15:25:27.000Z",
+    "ProductIdProduct": 3,
+    "CaracteristicIdCaracteristic": 1
+  },
+  {
+    "idProductCaracteristic": 38,
+    "value_caracteristic": "5",
+    "createdAt": "2021-06-11T15:26:29.000Z",
+    "updatedAt": "2021-06-11T15:26:29.000Z",
+    "ProductIdProduct": 3,
+    "CaracteristicIdCaracteristic": 4
+  }
+]
+*/
+
+/**
+for(let n = 0; n <= result.length - 1; n++) {
+                    for(let o = 0; o <= result[n].Caracteristics.length - 1; o++) {
+                        var allCaracteristicsProduct = await ProductCaracteristic.findAll({
+                            where: {
+                                ProductIdProduct: result[n].id_product,
+                                CaracteristicIdCaracteristic: result[n].Caracteristics[o].id_caracteristic
+                            }
+                        });
+                        for(let p = 0; p <= allCaracteristicsProduct.length - 1; p++) {
+                            if(p === 0) {
+                                result[n].Caracteristics[o].values_caracteristic = [allCaracteristicsProduct[p].value_caracteristic];
+                            } else {
+                                result[n].Caracteristics[o].values_caracteristic.push(allCaracteristicsProduct[p].value_caracteristic);
+                            }
+                        }
+                        // res.send(result[n].Caracteristics[o].values_caracteristic);
+                    }
+                }
+*/
