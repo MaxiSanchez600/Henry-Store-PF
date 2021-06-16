0a4be49e93277c1229b1cf0832ce113478bc4949
Josué Goyret <fjgoyretsola@gmail.com>
2021-06-16 12:46:08 -0300

    adds getCaracteristic controller

--- api/src/routes/infoFrontRoute.js
+++ api/src/routes/infoFrontRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { getDocumentTypes, getNacionalities, getCategories } = require('../controllers/getterInfoToFront.js');
+const { getDocumentTypes, getNacionalities, getCategories, getCaracteristic } = require('../controllers/getterInfoToFront.js');
 
 const router = Router();
 
 router.get('/documenttypes',getDocumentTypes);
 router.get('/nacionalities',getNacionalities);
-router.get('/categories', getCategories)
+router.get('/categories', getCategories);
+router.get('/caracteristics', getCaracteristic);
 
 module.exports = router;
