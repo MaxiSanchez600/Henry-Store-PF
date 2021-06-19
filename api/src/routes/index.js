e735f60a80c052ad3841c348374dbfa1d881ff05
Josué Goyret <fjgoyretsola@gmail.com>
2021-06-19 15:52:03 -0300

    comments lines related to register and login so backend server do not crash

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const authRoute = require('./auth.js');
-const infoFrontRoute = require('./infoFrontRoute.js');
 const productRouter = require('./product');
 
 // Importar todos los routers;
