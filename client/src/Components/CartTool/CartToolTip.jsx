08a5ff51866770f76809fa5f7b689add023c08e2
MaxiSanchez600 <maxsanchezg@gmail.com>
2021-06-22 23:07:43 -0300

    waasdasd

---
+++ client/src/Components/CartTool/CartToolTip.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+export default function CartToolTip(){
+    return(
+        <div>
+            <h1>AAAAAAA</h1>
+            <h1>AAAAAAA</h1>
+            <h1>AAAAAAA</h1>
+            <h1>AAAAAAA</h1>
+            <h1>AAAAAAA</h1>
+            <h1>AAAAAAA</h1>
+            <h1>AAAAAAA</h1>
+        </div>
+    )
+}
