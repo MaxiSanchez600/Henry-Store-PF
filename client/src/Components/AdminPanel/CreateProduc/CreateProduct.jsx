22e1e0a5f9ac962650e01f174bd683d92f68455b
Josué Goyret <fjgoyretsola@gmail.com>
2021-06-24 19:09:39 -0300

    works but needs validations

--- client/src/Components/AdminPanel/CreateProduc/CreateProduct.jsx
+++ client/src/Components/AdminPanel/CreateProduc/CreateProduct.jsx
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
-import {useSelector, useDispatch} from "react-redux";
 import CategoriesSelected from "../CategoriesSelected/CategoriesSelected";
 import CaracteristicsSelected from "../CaracteristicsSelected/CaracteristicsSelected";
-import CreateCaracteriscs from "../CreateCaracteristics/CreateCaracteriscs";
 import Tags from "../Tags/Tags";
 import ImageUploader from "../ImagesUploader/ImagesUploader";
 import './CreateProduct.scss'
-import {postProduct} from '../../../Redux/actions/actionsProducts'
 import axios from "axios"
 import Swal from 'sweetalert2';
 
-function CreateProduct ({editIsActive, productData, title}){
+function CreateProduct ({ editIsActive, title }) {
     const [catBack, setCatBack] = useState([]);
     const [carBack, setCarBack] = useState([]);
-    const dispatch = useDispatch()
     const [json, setJson] = useState({
         infoProduct: {
             name:'',
@@ -86,57 +82,55 @@ function CreateProduct ({editIsActive, productData, title}){
         
     },[]);
 
-    const onClickCreateCategory = async ()=>{
+    const onClickCreateCategory = async () => {
         const { value: category } = await Swal.fire({
             title: 'Añade una categoría',
             input: 'text',
             inputLabel: 'Nombre:',
             showCancelButton: true,
-            inputValidator: (value) => {
+            inputValidator: value => {
                 if (!value) {
-                return '¡Debe digitar un nombre para la catergoria!'
+                    return '¡Debe digitar un nombre para la catergoria!';
                 }
             }
-            })
-    
-            if (category) {
-                setCatBack([
-                    ...catBack,
-                    {
-                        name_category: category,
-                        SubCategories: []
-                    }
-                ]);
-            Swal.fire(`La categoria "${category}" fue añadida con éxito.`)
-            }
-    }
+        });
+        if (category) {
+            setCatBack([
+                ...catBack,
+                {
+                    name_category: category,
+                    SubCategories: []
+                }
+            ]);
+            // Swal.fire(`La categoria "${category}" fue añadida con éxito.`);
+        }
+    };
 
-    const onClickCreateCaracteristic = async ()=>{
+    const onClickCreateCaracteristic = async () => {
         const { value: caracteristic } = await Swal.fire({
             title: 'Añade una caracteristica',
             input: 'text',
             inputLabel: 'Nombre:',
             showCancelButton: true,
-            inputValidator: (value) => {
+            inputValidator: value => {
                 if (!value) {
-                return '¡Debe digitar un nombre para la caracteristica!'
+                    return '¡Debe digitar un nombre para la caracteristica!';
                 }
             }
-            })
-    
-            if (caracteristic) {
-                setCarBack([
-                    ...carBack,
-                    {
-                        name_caracteristic: caracteristic,
-                        values_caracteristic: []
-                    }
-                ]);
-            Swal.fire(`La categoria "${caracteristic}" fue añadida con éxito.`)
-            }
-    }
+        });
+        if (caracteristic) {
+            setCarBack([
+                ...carBack,
+                {
+                    name_caracteristic: caracteristic,
+                    values_caracteristic: []
+                }
+            ]);
+            // Swal.fire(`La categoria "${caracteristic}" fue añadida con éxito.`);
+        }
+    };
 
-    const onChangeInputs = (e) => {
+    const onChangeInputs = e => {
         setJson({
             ...json, 
             infoProduct: {
@@ -146,7 +140,7 @@ function CreateProduct ({editIsActive, productData, title}){
         });
     };
 
-    const onClickAddCategory = (e) => {
+    const onClickAddCategory = e => {
         setJson({
             ...json,
             categories: {
@@ -154,7 +148,7 @@ function CreateProduct ({editIsActive, productData, title}){
                 [e.target.name]: []
             }
         });
-    }
+    };
 
     const handleAddCaracteristic = e => {
         setJson({
@@ -164,18 +158,46 @@ function CreateProduct ({editIsActive, productData, title}){
                 [e.target.name] : []
             }
         });
-    }
+    };
 
-    const creacteProduct =()=>{
-/*         setJson({...json,
-            categories:subCatSelected,
-            tags:tags
-        }) */
-        dispatch(postProduct(json))
-    }
-    const removeCaracteristic = ()=>{
-        setJson({...json, caracteristics:{}})
-    }
+    const creacteProduct = () => {
+        axios.post("http://localhost:3001/product", json)
+        .then(res => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Producto creado con éxito',
+                confirmButtonText: `OK`
+              })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    setJson({
+                        infoProduct: {
+                            name:'',
+                            price:'',
+                            description:'',
+                            unit_stock:'',
+                            henry_coin:'',
+                            weight:'',
+                            size:'',
+                            percentage_discount:'',  
+                        },
+                        categories: {},
+                        caracteristics: {},
+                        tags: [],
+                        images: []
+                    });
+                }
+            });
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Algo salió mal, vuelve a intentar',
+                confirmButtonText: `OK`,
+            });
+        });
+    };
+    
 
     return(
         <div className='createContainer'>
@@ -250,14 +272,17 @@ function CreateProduct ({editIsActive, productData, title}){
                             <div>Son las propiedades que posee un producto, por ejemplo talla, color, etc. <span onClick={onClickCreateCaracteristic} className='addCaracteristic'>(Agregar caracteristica)</span></div>
                             <ul>
                                 {
-                                    carBack?.map((car, index)=>(
-                                        <button 
-                                            className='buttonCaracteristic' 
-                                            name={car.name_caracteristic} 
-                                            onClick={handleAddCaracteristic} 
-                                            key={index}>
-                                        {car.name_caracteristic}</button>
-                                    )) 
+                                    carBack?.map((car, index) => {
+                                        if(car.name_caracteristic !== 'type') {
+                                                return <button 
+                                                    className='buttonCaracteristic' 
+                                                    name={car.name_caracteristic} 
+                                                    onClick={handleAddCaracteristic} 
+                                                    key={index}>
+                                                {car.name_caracteristic}</button>
+                                            }
+                                        }
+                                    ) 
                                 }
                             </ul>
                             <CaracteristicsSelected 
