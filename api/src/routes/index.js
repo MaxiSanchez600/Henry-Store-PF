1c0c56cdfbdb711471f0e43095c8b5f087b9ebd6
Josué Goyret <fjgoyretsola@gmail.com>
2021-06-26 17:25:21 -0300

    adds review router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ const cartsRoute = require('./cart')
 const productRouter = require('./product');
 const userRoute = require ('./userFlow.js');
 const adminRoute = require ('./admin.js')
+const reviewRouter = require('./review');
 // Importar todos los routers;
 const router = Router();
 
@@ -13,5 +14,6 @@ router.use('/auth', authRoute)
 router.use('/cart', cartsRoute)
 router.use('/userflow',userRoute)
 router.use('/admin',adminRoute)
+router.use('/review', reviewRouter);
 
 module.exports = router;
