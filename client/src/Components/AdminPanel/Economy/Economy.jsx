7da4784331f5412fb22c9d66ca03f82b382b9a55
MaxiSanchez600 <maxsanchezg@gmail.com>
2021-07-02 01:51:35 -0300

    definitivo

--- client/src/Components/AdminPanel/Economy/Economy.jsx
+++ client/src/Components/AdminPanel/Economy/Economy.jsx
@@ -10,6 +10,7 @@ export default function Economy () {
         CLP: {value:""},
         MXN: {value:""},
         COP: {value:""},
+        USD: {value:""},
     }
     const [exchanges,setExchanges] = useState([])
     const [henrycoin,setHenrycoin] = useState()
